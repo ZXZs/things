2260c0bcf1dd561926036ae382ed41ec176339c4
Vladislav Kadun <double-vh@yandex.ru>
2021-02-09 11:02:35 +0600

    Update zadachees.py

--- competetive/zadachees.py
+++ competetive/zadachees.py
@@ -1,3 +1,16 @@
 # Task A
 [A, B, C, D] = list(map(int, input().split()))
 print(A * B * C * D)
+
+# Task B
+k = int(input())
+c = [0, None]
+n = 1
+
+while k != c[0]:
+    x = n ** 2
+    if x % 100 == 25:
+        c = [c[0]+1, x]
+    n += 1
+
+print(c[1])
