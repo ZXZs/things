97316d642cee6bcbcf6f0a0944345779a874823e
Vladislav Kadun <double-vh@yandex.ru>
2021-02-09 11:33:08 +0600

    Update zadachees.py

--- competetive/zadachees.py
+++ competetive/zadachees.py
@@ -16,3 +16,21 @@ while k != c1:
     n += 1
 
 print(c2)
+
+# Task C (вроде робит, но по примеру не робит ):)
+n = int(input())
+
+max_time = 5 * 60
+current_time = 0
+tasks_solved = 0
+
+for i in range(0, n):
+    times = list(map(int, input().split()))
+    m = min(times)
+    if current_time + m <= max_time:
+        current_time += m
+        tasks_solved += 1
+    if current_time == max_time:
+        break
+
+print(f"{tasks_solved} {current_time}")
